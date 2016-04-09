a7f813459ee180b979cb5c31cfd925fce4c81e76
JoaoFCosta <dinojoaocosta@gmail.com>
2016-04-09 14:01:39 +0100

    Initial Commit.

---
+++ Quotes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Quotes
+//
+//  Created by Joao Costa on 09/04/16.
+//  Copyright © 2016 Joao Costa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
