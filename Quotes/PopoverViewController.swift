56e386811ff9b214d6dde46b33a0c28a076a3e14
JoaoFCosta <dinojoaocosta@gmail.com>
2016-04-16 15:00:59 +0100

    Add navigation.
    - Add quotes navigation. The previous button is only displayed if the
      user is not on the first quote and it is now possible to navigate on
      the previous fetched quotes.

--- Quotes/PopoverViewController.swift
+++ Quotes/PopoverViewController.swift
@@ -13,12 +13,16 @@ class PopoverViewController: NSViewController, NSPopoverDelegate {
 	@IBOutlet weak var quoteTextField: NSTextField!
 	@IBOutlet weak var authorTextField: NSTextField!
 	@IBOutlet weak var playButton: NSButton!
+	@IBOutlet weak var previousButton: NSButton!
 	
 	let url: NSURL?	=
 		NSURL(string: "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json")
 	let playIcon: NSImage = NSImage(named: "PlayIcon")!
 	let stopIcon: NSImage = NSImage(named: "StopIcon")!
 	
+	var quotes: [String]	= [String]()
+	var authors: [String]	= [String]()
+	var quoteIndex: Int		= 0
 	var quote: String	= "Hello World" {
 		didSet { quoteTextField.stringValue	= quote }
 	}
@@ -39,6 +43,16 @@ class PopoverViewController: NSViewController, NSPopoverDelegate {
 		playButton.image	= playIcon
 	}
 	
+	override func viewWillLayout() {
+		super.viewWillLayout()
+		
+		// Fetch new quote to present before the view is loaded.
+		nextQuote(self)
+		
+		// Hide the previous button.
+		previousButton.hidden	= true
+	}
+	
 	// Quit Application.
 	@IBAction func quitApp(sender: AnyObject?) {
 		// Stop say task if it is currently running.
@@ -47,51 +61,76 @@ class PopoverViewController: NSViewController, NSPopoverDelegate {
 	}
 	
 	// Get a new quote and update the popover.
-	@IBAction func newQuote(sender: AnyObject?) {
-		if let url = url {
-			// Create a new data task to make a request to the API.
-			let session = NSURLSession.sharedSession()
-			session.dataTaskWithURL(url) {
-				[unowned self] (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-				if let data = data, JSONString = NSString(data: data, encoding: NSUTF8StringEncoding) {
-					// Remove '\' from the quoteText field to prevent JSON Serialization from failing.
-					let fixedString	=
-						JSONString.stringByReplacingOccurrencesOfString("\\", withString: "") as NSString
-					let fixedData		= fixedString.dataUsingEncoding(NSUTF8StringEncoding)
-					
-					do {
-						// Try to convert data to JSON and extract the author and the quote.
-						let json = try NSJSONSerialization.JSONObjectWithData(fixedData!,
-							options: NSJSONReadingOptions())
+	@IBAction func nextQuote(sender: AnyObject?) {
+		previousButton.hidden	= false
+		quoteIndex	+= 1
+		
+		if quoteIndex < quotes.count {
+			quote				= quotes[quoteIndex]
+			author			= authors[quoteIndex]
+		} else {
+			if let url = url {
+				// Create a new data task to make a request to the API.
+				let session = NSURLSession.sharedSession()
+				session.dataTaskWithURL(url) {
+					[unowned self] (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+					if let data = data, JSONString = NSString(data: data, encoding: NSUTF8StringEncoding) {
+						// Remove '\' from the quoteText field to prevent JSON Serialization from failing.
+						let fixedString	=
+							JSONString.stringByReplacingOccurrencesOfString("\\", withString: "") as NSString
+						let fixedData		= fixedString.dataUsingEncoding(NSUTF8StringEncoding)
 						
-						// Get the quote text from the JSON, trimming it.
-						if let jsonQuote = json["quoteText"] as? String {
-							self.quote = jsonQuote
-							.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-							.stringByReplacingOccurrencesOfString("\\", withString: "")
-							self.quote = "\"\(self.quote)\""
-						} else { self.quote = "\"Something went wrong :/\"" }
-						
-						// Get the quote author from the JSON, trimming it.
-						if let jsonAuthor = json["quoteAuthor"] as? String {
-							self.author = jsonAuthor
-  							.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+						do {
+							// Try to convert data to JSON and extract the author and the quote.
+							let json = try NSJSONSerialization.JSONObjectWithData(fixedData!,
+							                                                      options: NSJSONReadingOptions())
 							
-							if self.author == "" { self.author = "Unknown" }
+							// Get the quote text from the JSON, trimming it.
+							if let jsonQuote = json["quoteText"] as? String {
+								self.quote = jsonQuote
+									.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+									.stringByReplacingOccurrencesOfString("\\", withString: "")
+								self.quote = "\"\(self.quote)\""
+							} else { self.quote = "\"Something went wrong :/\"" }
+							
+							// Get the quote author from the JSON, trimming it.
+							if let jsonAuthor = json["quoteAuthor"] as? String {
+								self.author = jsonAuthor
+  							.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+								
+								if self.author == "" { self.author = "Unknown" }
+							}
+							
+							// Save the new quote.
+							self.quotes.append(self.quote)
+							self.authors.append(self.author)
+							self.quoteIndex = self.quotes.count
+						} catch {
+							// Failure reading JSON.
+							self.quote	= "Something went wrong! Try again."
+							self.author	= "This App"
 						}
-					} catch {
-						// Failure reading JSON.
-						self.quote	= "Something went wrong! Try again."
-						self.author	= "This App"
+					} else if error != nil {
+						// No Internet Connection.
+						if !self.connected {
+							self.quote	= "\"It seems that you are not connected to the internet!\""
+							self.author	= "This App"
+						}
 					}
-				} else if error != nil {
-					// No Internet Connection.
-					if !self.connected {
-						self.quote	= "\"It seems that you are not connected to the internet!\""
-						self.author	= "This App"
-					}
-				}
-			}.resume()
+					}.resume()
+			}
+			
+		}
+	}
+	
+	@IBAction func previousQuote(sender: AnyObject?) {
+		if quoteIndex != 0 {
+			quoteIndex -= 1
+			
+			quote		= quotes[quoteIndex]
+			author	= authors[quoteIndex]
+			
+			if quoteIndex	== 0 { previousButton.hidden = true }
 		}
 	}
 	
